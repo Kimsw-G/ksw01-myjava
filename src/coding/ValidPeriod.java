af32a94267f8b6a16b18856188dc796d2330797e
Kimsw-G <kimsw3445@naver.com>
2023-02-09 21:59:31 +0900

    complete ValidPeriod

--- src/coding/ValidPeriod.java
+++ src/coding/ValidPeriod.java
@@ -10,7 +10,7 @@ public class ValidPeriod {
     public static void main(String[] args) {
         String today = "2022.05.19";
         String[] terms = {"A 6", "B 12", "C 3"};
-        String[] pre = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
+        String[] pre = {"2021.12.02 B", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
         ValidPeriodSolution vps = new ValidPeriodSolution();
         System.out.println(Arrays.toString(vps.solution(today, terms, pre)));
     }
@@ -19,18 +19,28 @@ public class ValidPeriod {
 class ValidPeriodSolution{
     public int[] solution(String today, String[] terms, String[] privacies) {
         int[] answer;
+
         List<Integer> answerList = new ArrayList<Integer>();
         Map<String,Integer> map = termsToMap(terms);
-        today = today.replace(".", "");
+
+        int currentYear = Integer.parseInt(today.substring(0, 4));
+        int currentMonth = Integer.parseInt(today.substring(5, 7));
+        int currentDay = Integer.parseInt(today.substring(8, 10));
         for (int i = 0; i < privacies.length; i++) {
             String expiredDay = calcExpiredDay(privacies[i], map);
-            System.out.println(Integer.parseInt(today) - Integer.parseInt(expiredDay));
-            System.out.println(Integer.parseInt(today));
-            System.out.println(Integer.parseInt(expiredDay));
-            if(Integer.parseInt(today) - Integer.parseInt(expiredDay)>0){
-                answerList.add(i+1);
+            int expYear = Integer.parseInt(expiredDay.substring(0, 4));
+            int expMonth = Integer.parseInt(expiredDay.substring(4, 6));
+            int expDay = Integer.parseInt(expiredDay.substring(6, 8));
+            // System.out.print(expYear+" ");
+            // System.out.print(expMonth+" ");
+            // System.out.println(expDay);
+            if (currentYear > expYear || 
+            (currentYear == expYear && currentMonth > expMonth) || 
+            (currentYear == expYear && currentMonth == expMonth && currentDay > expDay)) {
+                answerList.add(i + 1);
             }
         }
+
         answer = new int[answerList.size()];
         for (int i = 0; i < answer.length; i++) {
             answer[i] = answerList.get(i);
@@ -55,10 +65,11 @@ class ValidPeriodSolution{
         Integer term = map.get(privacie.substring(11));
 
         month+=term;
-        if(month>12){
-            month-=12;
+        while(month>12){
             year+=1;
+            month-=12;
         }
+
         day-=1;
         if(day<=0){
             day+=28;
